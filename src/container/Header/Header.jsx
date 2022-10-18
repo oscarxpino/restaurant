4d5bb70915ae81bd5f50dfe0237093417ff23f59
Oscar Pino <oscar.pino17@tectijuana.edu.mx>
2022-10-17 23:14:52 -0700

    update1

--- src/container/Header/Header.jsx
+++ src/container/Header/Header.jsx
@@ -7,7 +7,9 @@ import './Header.css';
 const Header = () => (
   <div className='app__header app_wrapper section__padding' id='home'>
     <div className='app__wrapper_info'>
-      <SubHeading/>
+      <SubHeading title='Chase the new flavour'/>
+      <h1 className='app__header-h1'>The Key to Fine Dining</h1>
+      <p className='p__opensans' style={{margin:'2rem 0'}}>Descripcion acerca de la pizzeria galgos</p>
     </div>
     <div className='app__wrapper_img'>
     
